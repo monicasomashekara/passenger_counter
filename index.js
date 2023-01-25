aca2296c1b8657e79012b9c2ba690d10550f9c15
monicasomashekara <70906410+monicasomashekara@users.noreply.github.com>
2023-01-25 15:16:35 -0800

    first passenger counter
    first passenger counter

---
+++ index.js
@@ -0,0 +1,12 @@
+// Javascript
+//document.getElementById("count-el").innerText=5
+
+let count = 6
+console.log(count)
+
+let myAge = 30
+console.log(myAge)
+
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
